28d5fd987db2a2472c11ae66df52dc92a67c7eba
Abhishek kumar <abhi20040805@gmail.com>
2026-03-12 12:13:06 +0530

    Refactor import statement for express module

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import express from "express";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
